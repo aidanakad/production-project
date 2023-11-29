4efa9b9ec2a7a16bf2f008592b590a3d296171f2
aidanakad <aydana0504@gmail.com>
2023-11-29 19:13:18 +0600

    add jest

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { HomePage } from 'pages/HomePage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFound } from 'pages/NotFound';
 
 export enum AppRoutes{
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RouterPath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RouterPath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RouterPath.not_found,
+        element: <NotFound />,
     },
 };
